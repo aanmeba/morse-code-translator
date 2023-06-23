d73e0459e24b873b2df7669ac194551808a29e95
aanmeba <aanmeba@gmail.com>
2023-06-23 15:32:31 +1000

    setup test suit

---
+++ js/translator.js
@@ -0,0 +1,5 @@
+export const inputValidator = () => {};
+export const engToMorse = () => {};
+export const morseToEng = () => {};
+export const readJSON = () => {};
+const runTranslator = () => {};
---
+++ js/translator.test.js
@@ -0,0 +1,21 @@
+import {
+  readJSON,
+  inputValidator,
+  engToMorse,
+  morseToEng,
+} from "./translator.js";
+
+describe("Testing readJSON() function", () => {
+  // read the json file correctly
+});
+
+describe("Testing inputValidator() function", () => {
+  // check if the input is valid
+});
+
+describe("Testing engToMorse() function", () => {
+  // return a correct value
+});
+describe("Testing morseToEng() function", () => {
+  // return a correct value
+});
